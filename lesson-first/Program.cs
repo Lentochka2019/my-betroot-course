a90b6635895ca038c7644b35b46999f77b197382
Olena <elenapilenaolegovna@gmail.com>
2022-02-11 09:36:30 +0200

    lesson1

--- lesson-first/Program.cs
+++ lesson-first/Program.cs
@@ -4,9 +4,31 @@ namespace lesson_first
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        public void Caller()
         {
-            Console.WriteLine("homework/lesson1");
+            int numA = 4;
+            // Call with an int variable.
+            int productA = Square(numA);
+            Console.WriteLine($"Ex1: {productA}");
+
+            int numB = 32;
+            // Call with another int variable.
+            int productB = Square(numB);
+
+            // Call with an integer literal.
+            int productC = Square(12);
+
+            // Call with an expression that evaulates to int.
+            productC = Square(productA * 3);
+            Console.WriteLine($"Ex1: {productC}");
         }
+        
+        int Square(int i)
+        {      // Store input argument in a local variable.
+            int input = i;
+            return input * input;
+        }
+
     }
 }
